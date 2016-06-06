8367adb30df019203f042973e45a918a00930f3e
sdossettswift <sdossettswift@gmail.com>
2016-06-06 16:48:19 -0500

    update voices

--- bouncer.rb
+++ bouncer.rb
@@ -1,4 +1,5 @@
 def bouncer_greeting
+  `say 'Welcome to Dorsia!'`
   puts "
                           =======================
                           =  WELCOME TO DORSIA  =
@@ -33,12 +34,15 @@ def bouncer_greeting
                           :...........................~
                                                            "
 
+  `say 'Mind the velvet rope. You gotta wait, we are at capacity.'`
+
   puts "
   ===============================================================
   BOUNCER SAYS: Mind the velvet rope. You gotta wait, we are at
   capacity.
   =============================================================== "
-  sleep 3
+  `say 'No, I do not see your name on the VIP list. Get
+  back in line, or you and your friends will never get in.'`
   puts "
   ===============================================================
   BOUNCER SAYS: No, I do not see your name on the VIP list. Get
@@ -46,11 +50,12 @@ def bouncer_greeting
   ==============================================================="
   sleep 15
 
+  `say 'OK. Let me see your ID. How old are you?'`
+
   puts "
   ===============================================================
-  BOUNCER SAYS: OK. Let me see your ID
+  BOUNCER SAYS: OK. Let me see your ID. How old are you?
   ==============================================================="
-  puts "    How old are you?"
 
   age= gets.chomp.to_i
   return age
@@ -58,6 +63,7 @@ end
 
 def check_age (age)
   if  age >= 25
+      `say 'Over 25? You are too old to be here.'`
     "===============================================================
     BOUNCER SAYS:As you are old enough to rent a car, you are
     probably too old to come in here. I mean, you can come in
@@ -66,16 +72,19 @@ def check_age (age)
     crisis.
   ==============================================================="
     elsif age >=21
+        `say 'Right this way.'`
   "===============================================================
     BOUNCER SAYS:Come on in! Drink and smoke to your heart's
       delight. And don't forget to vote in tomorrow's primary.
   ==============================================================="
     elsif age >= 18
+        `say 'Your fake I D is no good here.'`
          "
   ===============================================================
     BOUNCER SAYS: You can smoke and vote, but you can't come in.
   ==============================================================="
     else
+        `say 'You are way too young.'`
          "
   ===============================================================
   BOUNCER SAYS: Go home. It is past your bedtime.
@@ -85,17 +94,20 @@ end
 
 def check_age_UK (age)
   if  age >=18
+      `say -v Bruce 'Cheery-O, Mate.'`
   "===============================================================
     BOUNCER SAYS: Cheery-O, Mate. Drink and smoke to your heart's
       delight.
   ==============================================================="
   elsif age >= 16
+      `say -v Bruce 'You can only drink if your parents are here.'`
            "
     ===============================================================
       BOUNCER SAYS: If you are with your parents, come on in.
       Otherwise, run on home.
     ==============================================================="
       else
+        `say -v Bruce 'Run along.'`
            "
     ===============================================================
     BOUNCER SAYS: Go home. It is past your bedtime.
@@ -104,6 +116,7 @@ def check_age_UK (age)
 end
 
 def sovereign
+  `say 'Are you under the rule of a president or monarch?'`
   puts "Are you under the rule of a president or a monarch?"
   puts "Enter P or M"
   response = gets.chomp.upcase
